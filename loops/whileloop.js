0f45728d0aaac8eff88b397481f1fb116f613cf2
sonubaraik <sonubaraik560@gmail.com>
2026-05-24 21:52:57 +0530

    While loop

---
+++ loops/whileloop.js
@@ -0,0 +1,30 @@
+// print number from 10 to 1
+
+// let i = 10;
+// while (i > 0) {
+//   console.log(i);
+//   i--;
+// }
+
+// print odd no from 1 to 20
+
+// let i=1;
+// while(i<=20){
+//     if(i%2!=0){
+//         console.log(i)
+//     }
+//     i++;
+// }
+
+// count digit in given number 12345678
+
+// let num=12345678;
+// console.log(num.toString().length)
+
+// Simulate battery charging from 10% to 100%
+
+let charge=0;
+while(charge<100){
+    charge=charge+10;
+    console.log("Your battery is charged",charge,"%")  
+}
