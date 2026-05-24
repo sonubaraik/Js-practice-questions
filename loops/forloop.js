7f8342abca226aec497dc865488a658c2b8f4d41
sonubaraik <sonubaraik560@gmail.com>
2026-05-24 15:09:14 +0530

    for loop

---
+++ loops/forloop.js
@@ -0,0 +1,33 @@
+// print no from 1 to 10
+
+// for(let i=1; i<=10; i++){
+//     console.log(i)
+// }
+
+// print even number from 1 to 20
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// // print tabe of 5
+// let num=5;
+// for(let i=1; i<=10; i++){
+//    let table= num*i;
+//     console.log(num, "*", i, "=" ,table)
+// }
+
+// table of 10
+
+// let num=10;
+// for(let i=1;i<=10;i++){
+//     console.log(`${num} * ${i} = ${num*i}`)
+// }
+
+for(let i=1;i<=10;i++){
+    for(let j=1;j<=10;j++){
+        console.log(`${i} * ${j} = ${i*j}`)
+    }
+}
