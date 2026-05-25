11f5d9ead81592050853368464cd7446c4910a34
sonubaraik <sonubaraik560@gmail.com>
2026-05-25 11:50:42 +0530

    if else

---
+++ if-else/if-else.js
@@ -0,0 +1,52 @@
+// check number is even or odd
+// let num = 23;
+// if (num % 2 === 0) {
+//   console.log(num, "is even number");
+// } else {
+//   console.log(num, "is odd number");
+// }
+
+// check if number is positive,negative or zero
+
+// let num=5;
+// if(num>0){
+//     console.log(num,"is positive number");
+// }else if (num === 0) {
+//     console.log(num,"is zero");
+// } else {
+//      console.log(num,"is negative number");
+    
+// }
+
+// traffic signal system
+
+// let trafficLight="red"
+// if(trafficLight==="red"){
+//     console.log("Stop");
+// }else if(trafficLight==="yellow"){
+//     console.log("Ready");
+// }else if(trafficLight==="green"){
+//     console.log("Go");
+// }
+  
+// weather sudggesion system
+
+// let weather="cold"
+// if(weather==="hot"){
+//     console.log("Drink water")
+// }else if(weather==="rainy"){
+//     console.log("Take umbrella")
+// }else if(weather==="cold"){
+//     console.log("Wear jacket")
+// }
+
+// Salary bonus calculation
+
+// let exp=8
+// if(exp>=10){
+//     console.log("You will get 20% bonus")
+// }else if(exp>=5){
+//     console.log("You will get 10% bonus")
+// }else{
+//     console.log("You will get 5% bonus")
+// }
