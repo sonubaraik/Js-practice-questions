4509b0ead466744bba774dad8ddfe9e885bc8099
sonubaraik <sonubaraik560@gmail.com>
2026-05-25 12:45:16 +0530

    nested if

---
+++ nested-if/nested.js
@@ -0,0 +1,37 @@
+// let tenthMarks=75;
+// let twelthMarks=70
+// if(twelthMarks>=60){
+//     if(tenthMarks>=70){
+//         console.log("You can take admision in Graduation");
+//     }else{
+//         console.log("You are not eligible for admission")
+//     }
+// }else{
+//     console.log("You are not eligible for admission")
+// }
+
+    //   or
+
+// let tenthMarks=75;
+// let twelthMarks=70
+
+// if(tenthMarks>=70 && twelthMarks>=60){
+//     console.log("You can take admision in Graduation");
+// }else{
+//     console.log("You are not eligible for admission")
+// }
+
+
+// bank loan approval
+
+let salary=40000;
+let civilScore=800;
+if(salary>=30000){
+    if(civilScore>=700){
+        console.log("Your loan application is approved")
+    }else{
+        console.log("Your loan application is rejected")  
+    }
+}else{
+    console.log("Your loan application is rejected")
+}
